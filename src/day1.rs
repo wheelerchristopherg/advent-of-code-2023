8c0d818ed20ceae40abcd53218a4c82d47a9a051
Christopher Wheeler <wheelerchristopherg@gmail.com>
2023-12-01 00:40:10 -0600

    day 1 complete

--- src/day1.rs
+++ src/day1.rs
@@ -1,9 +1,60 @@
+use std::char;
+
 pub fn part1(input: &[String]) -> i32 {
-    todo!()
+    let mut numbers: Vec<i32> = vec![];
+    for line in input {
+        let mut digits: Vec<char> = vec![];
+        for c in line.chars() {
+            if c.is_numeric() {
+                digits.push(c);
+            }
+        }
+        if !digits.is_empty() {
+            let num: String = format!("{}{}", digits.first().unwrap(), digits.last().unwrap());
+            numbers.push(num.parse::<i32>().unwrap());
+        }
+    }
+    numbers.iter().sum()
+}
+
+fn find_digits(input: &str) -> i32 {
+    let x: &[_] = &[
+        "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "1", "2", "3", "4",
+        "5", "6", "7", "8", "9",
+    ];
+    let mut first_occurance = (0, input.len());
+    let mut last_occurance = (0, 0);
+    for (i, &s) in x.iter().enumerate() {
+        let first = input.find(s);
+        let last = input.rfind(s);
+        if let (Some(f), Some(l)) = (first, last) {
+            if f <= first_occurance.1 {
+                // println!("{:?}", first);
+                first_occurance.0 = (i % 9) + 1;
+                first_occurance.1 = f;
+            }
+            if l >= last_occurance.1 {
+                // println!("{:?}", last);
+                last_occurance.0 = (i % 9) + 1;
+                last_occurance.1 = l;
+            }
+        }
+    }
+    // println!("first_occurance: {:?}", first_occurance);
+    // println!("last_occurance: {:?}", last_occurance);
+    (first_occurance.0 * 10 + last_occurance.0)
+        .try_into()
+        .unwrap()
 }
 
 pub fn part2(input: &[String]) -> i32 {
-    todo!()
+    let mut numbers: Vec<i32> = vec![];
+    for line in input {
+        let num = find_digits(line);
+        println!("{}: {}", num, line);
+        numbers.push(num);
+    }
+    numbers.iter().sum()
 }
 
 #[cfg(test)]
@@ -13,15 +64,30 @@ mod tests {
 
     #[test]
     fn test_part1() {
-        let input = read_input("./input/test.txt");
+        let input = read_input("./input/day1.txt");
         let result = part1(&input);
         println!("Result: {result}");
     }
 
     #[test]
     fn test_part2() {
-        let input = read_input("./input/test.txt");
+        let input = read_input("./input/day1.txt");
         let result = part2(&input);
+        println!("Result: {result}");
+    }
+
+    #[test]
+    fn test_line() {
+        let input = "gxjzhvkbcjhscdxhjdqxnhsevenxrdrjbcl5fvlvlxjjvb";
+        let result = find_digits(input);
+        println!("Result: {result}");
+    }
+
+
+    #[test]
+    fn test_line1() {
+        let input = "5dxcsvgqkmz";
+        let result = find_digits(input);
         println!("Result: {result}");
     }
 }
