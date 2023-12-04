0d63dd846d755eb2a299229c1929a128154970ab
Christopher Wheeler <wheelerchristopherg@gmail.com>
2023-12-03 21:24:19 -0600

    use alternative datastructure to improve speed/because I could

--- src/day3.rs
+++ src/day3.rs
@@ -139,7 +139,151 @@ pub fn part2(input: &[String]) -> i32 {
                             .parse::<i32>()
                             .unwrap();
                         gear_count += 1;
-                        println!("{} {}", gear_count, part_number);
+                        gear_ratio *= part_number;
+                    }
+                }
+                if gear_count == 2 {
+                    gear_ratio_sum += gear_ratio;
+                }
+            }
+        }
+    }
+    gear_ratio_sum
+}
+
+#[derive(Debug)]
+struct GridCell {
+  visited: bool,
+  value: char,
+}
+
+
+
+pub fn part1_no_set(input: &[String]) -> i32 {
+    let mut grid: Vec<Vec<GridCell>> = input.iter().map(|x| x.chars().map(|x| GridCell{visited:false, value:x}).collect()).collect();
+    let length = grid[0].len();
+    let adjacent_positions: [(i32, i32);8] = [
+        (-1, -1),
+        (0, -1),
+        (1, -1),
+        (-1, 0),
+        (1, 0),
+        (-1, 1),
+        (0, 1),
+        (1, 1),
+    ];
+    let mut part_number_sum = 0;
+    for y in 0..grid.len() {
+        for x in 0..grid[0].len() {
+            let c: char = grid[y][x].value;
+            if !c.is_numeric() && c != '.' {
+                for &(x1, y1) in adjacent_positions.iter() {
+                    let x2: usize = ((x as i32) + x1) as usize;
+                    let y2: usize = ((y as i32) + y1) as usize;
+                    if grid[y2][x2].visited {
+                        continue;
+                    }
+                    let c: char = grid[y2][x2].value;
+                    if c.is_numeric() {
+                        grid[y2][x2].visited = true;
+                        let mut i: i32 = (x2 as i32) - 1;
+                        let mut left_bound: usize = 0;
+                        let mut right_bound: usize = length;
+                        while i >= 0 {
+                            if grid[y2][i as usize].value.is_numeric()
+                            {
+                                grid[y2][i as usize].visited = true;
+                                i -= 1;
+                            } else {
+                                left_bound = (i + 1) as usize;
+                                break;
+                            }
+                        }
+                        i = (x2 as i32) + 1;
+                        while (i as usize) < grid[0].len() {
+                            if grid[y2][i as usize].value.is_numeric()
+                            {
+                                grid[y2][i as usize].visited = true;
+                                i += 1;
+                            } else {
+                                right_bound = i as usize;
+                                break;
+                            }
+                        }
+                        let mut part_number: String = String::new();
+                        for c in grid[y2][left_bound..right_bound].iter() {
+                            part_number.push(c.value);
+                        }
+
+                        part_number_sum += part_number.parse::<i32>().unwrap();
+                    }
+                }
+            }
+        }
+    }
+    part_number_sum
+}
+
+pub fn part2_no_set(input: &[String]) -> i32 {
+    let mut grid: Vec<Vec<GridCell>> = input.iter().map(|x| x.chars().map(|x| GridCell{visited:false, value:x}).collect()).collect();
+    let length = grid[0].len();
+    let adjacent_positions: [(i32, i32);8] = [
+        (-1, -1),
+        (0, -1),
+        (1, -1),
+        (-1, 0),
+        (1, 0),
+        (-1, 1),
+        (0, 1),
+        (1, 1),
+    ];
+    let mut gear_ratio_sum = 0;
+    for y in 0..grid.len() {
+        for x in 0..grid[0].len() {
+            let c: char = grid[y][x].value;
+            if c == '*' {
+                let mut gear_count = 0;
+                let mut gear_ratio = 1;
+                for &(x1, y1) in adjacent_positions.iter() {
+                    let x2: usize = ((x as i32) + x1) as usize;
+                    let y2: usize = ((y as i32) + y1) as usize;
+                    if grid[y2][x2].visited {
+                        continue;
+                    }
+                    let c: char = grid[y2][x2].value;
+                    if c.is_numeric() {
+                        grid[y2][x2].visited = true;
+                        let mut i: i32 = (x2 as i32) - 1;
+                        let mut left_bound: usize = 0;
+                        let mut right_bound: usize = length;
+                        while i >= 0 {
+                            if grid[y2][i as usize].value.is_numeric()
+                            {
+                                grid[y2][i as usize].visited = true;
+                                i -= 1;
+                            } else {
+                                left_bound = (i + 1) as usize;
+                                break;
+                            }
+                        }
+                        i = (x2 as i32) + 1;
+                        while (i as usize) < grid[0].len() {
+                            if grid[y2][i as usize].value.is_numeric()
+                            {
+                                grid[y2][i as usize].visited = true;
+                                i += 1;
+                            } else {
+                                right_bound = i as usize;
+                                break;
+                            }
+                        }
+                        let mut part_number: String = String::new();
+                        for c in grid[y2][left_bound..right_bound].iter() {
+                            part_number.push(c.value);
+                        }
+
+                        let part_number = part_number.parse::<i32>().unwrap();
+                        gear_count += 1;
                         gear_ratio *= part_number;
                     }
                 }
@@ -168,6 +312,20 @@ mod tests {
     fn test_part2() {
         let input = read_input("./input/day3.txt");
         let result = part2(&input);
+        println!("Result: {result}");
+    }
+
+    #[test]
+    fn test_part1_no_set() {
+        let input = read_input("./input/day3.txt");
+        let result = part1_no_set(&input);
+        println!("Result: {result}");
+    }
+
+    #[test]
+    fn test_part2_no_set() {
+        let input = read_input("./input/day3.txt");
+        let result = part2_no_set(&input);
         println!("Result: {result}");
     }
 }
